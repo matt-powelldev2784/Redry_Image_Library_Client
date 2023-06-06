23ff88b0fe63833fe12fdf747a868cfbb7d2b0e5
Matthew Powell <matt.powell2784@gmail.com>
2023-06-06 18:29:55 +0100

    add base settings to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryGreen: '#00B200',
+        lightGrey: '#EEEEEE',
+        darkGrey: ' #D9D9D9',
+        quaternaryGrey: '#cfd7d7',
+      },
+      screens: {
+        sm: '0px',
+        md: '600px',
+        lg: '1100px',
+      },
+    },
   },
   plugins: [],
 }
