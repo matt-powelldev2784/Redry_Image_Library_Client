776bbeb5342e49ca5f1c83e794c2c53d1146892f
Matthew Powell <matt.powell2784@gmail.com>
2023-06-07 17:05:50 +0100

    responsive design added to homepage

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,27 +1,34 @@
 export const Hero = () => {
   return (
-    <section className="relative flex h-screen w-screen items-center justify-start">
+    <section className="relative flex h-screen w-screen min-w-[320px] items-center justify-center">
       <img
-        src="/world_green_light_full.jpg"
+        src="/world_green_light_extended4.jpg"
         alt="Green leaves background"
         className="absolute h-full w-full object-cover"
       />
-      <div className="z-10 ml-32 flex w-[40rem] flex-col items-start justify-start gap-2">
-        <h1 className="text-7xl font-semibold">RE:DRY</h1>
-        <p className="text-3xl">Capture the world in a new light.</p>
-        <p className="text-justify text-xl">
-          Welcome to Redry - your destination for stunning, high-quality images.
-          Explore our collection and find the perfect visuals for your next
-          project.
+      <div className="z-20 flex w-full flex-col items-center justify-center lg:mx-16 lg:w-[40rem]">
+        <img
+          src="/re_dry_logo_green_slogan.svg"
+          alt=""
+          className="m-4 h-20 md:h-24"
+        />
+        <p className="mt-8 text-center text-lg font-semibold md:text-3xl">
+          User Generated Image Library
         </p>
-
+        <p className="mx-4 mt-4 text-center text-base md:w-[500px] md:text-xl lg:mx-0">
+          Your destination for stunning, high-quality images. Explore our
+          collection and find the perfect visuals for your next project.
+        </p>
         <input
-          className="mt-4 w-[28rem] rounded-xl px-3 py-2 text-base"
+          className="mt-12 w-11/12 rounded-xl px-3 py-2 text-center text-base md:w-1/2 lg:w-[28rem]"
           placeholder="Search Image Library"
         ></input>
-        <button className="my-2 rounded-xl bg-primaryGreen px-4 py-1 font-semibold text-darkBlack">
+        <button className="my-2 mt-4 rounded-xl border-2 border-darkBlack bg-primaryGreen px-4 py-1 text-xl font-semibold text-darkBlack lg:w-[28rem]">
           Search
         </button>
+        {/* <p className="text-3xl sm:text-red-500 md:text-blue-500 lg:text-orange-500">
+          TEXT
+        </p> */}
       </div>
     </section>
   )
--- src/components/navBar/Nav.tsx
+++ src/components/navBar/Nav.tsx
@@ -1,10 +1,10 @@
 export const Nav = () => {
   return (
-    <div className="fixed z-50 flex h-20 w-full items-center justify-between">
-      <img src="/re_dry_logo_green.svg" alt="" className="m-4 h-12" />
-      <button className="m-4 rounded-xl bg-primaryGreen px-4 py-1 font-semibold text-darkBlack">
-        Upload Photo
-      </button>
+    <div className="fixed z-50 flex h-20 w-full items-center justify-center gap-4">
+      <p className="p-1 px-2">Developer API</p>
+      <p className="rounded border-2 border-darkBlack/75 p-1 px-2">
+        Upload Image
+      </p>
     </div>
   )
 }
