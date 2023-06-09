2f6f8d9868957b46ea6a6864330d24ab0e62344c
Matthew Powell <matt.powell2784@gmail.com>
2023-06-09 14:18:31 +0100

    tests refactored

--- src/components/hero/__tests__/hero.test.tsx
+++ src/components/hero/__tests__/hero.test.tsx
@@ -8,16 +8,21 @@ describe('Hero', () => {
     expect(
       screen.getByAltText('Green light bulb background')
     ).toBeInTheDocument()
+
     expect(screen.getByAltText('Redry logo with slogan')).toBeInTheDocument()
+
     expect(screen.getByText('User Generated Image Library')).toBeInTheDocument()
+
     expect(
       screen.getByText(
         'Your destination for stunning, high-quality images. Explore our collection and find the perfect visuals for your next project.'
       )
     ).toBeInTheDocument()
+
     expect(
       screen.getByPlaceholderText('Search Image Library')
     ).toBeInTheDocument()
+
     expect(screen.getByText('Search')).toBeInTheDocument()
   })
 })
