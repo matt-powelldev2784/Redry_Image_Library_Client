1297e041409de33800612b6e8fa6223db27d894b
Matthew Powell <matt.powell2784@gmail.com>
2023-06-09 14:17:22 +0100

    env file added to git ignore

--- src/components/utils/apiCall.ts
+++ src/components/utils/apiCall.ts
@@ -3,7 +3,7 @@ import { ApiOptions } from '../../TS/interfaces'
 
 export const apiCall = async (apiOptions: ApiOptions) => {
   const { httpMethod, route, body } = apiOptions
-  const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || process.env.BASE_URL
+  const BASE_URL = process.env.BASE_URL
 
   try {
     let response: any
