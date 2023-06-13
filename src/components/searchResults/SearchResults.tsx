d4019789075a1ee43d7a999bd0947669bfd90bae
Matthew Powell <matt.powell2784@gmail.com>
2023-06-13 19:59:01 +0100

    add search term to search results

--- src/components/searchResults/SearchResults.tsx
+++ src/components/searchResults/SearchResults.tsx
@@ -4,6 +4,9 @@ import { SearchInput } from '../ui/ui-index'
 
 export const SearchResults = () => {
   const searchResults = useAppSelector((state) => state.dataReducer.imageData)
+  const currentSearchTerm = useAppSelector(
+    (state) => state.dataReducer.searchTerm
+  )
 
   const imageItems = searchResults.map((imageData) => {
     const { _id } = imageData
@@ -16,7 +19,9 @@ export const SearchResults = () => {
         optionalClassNames="mt-12 w-11/12 bg-slate-200 rounded-xl px-3 py-2 text-center text-base md:w-1/2 lg:w-[28rem]"
         placeholderText="Search Image Library"
       />
-      <h1 className="mt-16 text-3xl">Serach Results for: </h1>
+      <h1 className="mt-16 text-3xl">
+        Serach Results for: {currentSearchTerm}
+      </h1>
       <div className="flex w-screen items-center justify-center">
         <div className="mb-12 mt-12 flex w-full flex-wrap items-center justify-center gap-8 md:mx-4 lg:mx-8 lg:gap-14">
           {imageItems}
