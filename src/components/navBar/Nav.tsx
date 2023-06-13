346cf1a88fbed1f0f40049dd319841028bda8d9b
Matthew Powell <matt.powell2784@gmail.com>
2023-06-13 19:52:11 +0100

    change nav bar to absolute position

--- src/components/navBar/Nav.tsx
+++ src/components/navBar/Nav.tsx
@@ -1,6 +1,6 @@
 export const Nav = () => {
   return (
-    <div className="fixed z-50 flex h-20 w-full items-center justify-center gap-4 bg-lightGrey/25">
+    <div className="absolute z-50 flex h-20 w-full items-center justify-center gap-4 bg-lightGrey/25">
       <p className="p-1 px-2">Developer API</p>
       <p className="rounded border-2 border-darkBlack/75 p-1 px-2">
         Upload Image
