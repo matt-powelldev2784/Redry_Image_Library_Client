56aec4d1731dd0b0664958886c8162166a947e14
Matthew Powell <matt.powell2784@gmail.com>
2023-06-21 21:06:05 +0100

    add api error text

--- src/components/uploadImage/utils/Form.tsx
+++ src/components/uploadImage/utils/Form.tsx
@@ -2,15 +2,21 @@ import { useFormikProps } from './useFormik'
 import { DropFile } from './DropFile'
 import { Button } from '../../ui/ui-index'
 import { InputField } from './InputField'
+import { useAppSelector } from '../../../redux/hooks/reduxHooks'
 
 export const Form = () => {
   const formik = useFormikProps()
+  const { errors } = useAppSelector((state) => state.imageUploadReducer)
+
+  const errorsJSX = errors?.map((err) => {
+    return <p className="text-center text-red-500">Server Error: {err}</p>
+  })
 
   return (
     <article className="flex w-full items-center justify-center">
       <div className="justify-cente my-4 mb-8 flex flex-col items-center md:w-1/2">
         <h1 className="text-3xl">Upload Image</h1>
-
+        {errorsJSX ? errorsJSX : null}
         <form
           onSubmit={formik.handleSubmit}
           className="flex w-11/12 flex-col items-center justify-center md:w-full"
