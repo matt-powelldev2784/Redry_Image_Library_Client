ececadacd28d4340c050ac0a3ba7852ff7ce9bac
Matthew Powell <matt.powell2784@gmail.com>
2023-06-24 13:43:43 +0100

    serach results refactor

---
+++ src/components/searchResults/SearchResults.tsx
@@ -0,0 +1,27 @@
+import { useAppSelector } from '../../redux/hooks/reduxHooks'
+import { ImageItem } from './ImageItem'
+
+export const SearchResults = () => {
+  const searchResults = useAppSelector((state) => state.dataReducer.imageData)
+  const currentSearchTerm = useAppSelector(
+    (state) => state.dataReducer.searchTerm
+  )
+
+  const imageItems = searchResults.map((imageData) => {
+    const { _id } = imageData
+    return <ImageItem key={_id} imageData={imageData} />
+  })
+
+  return (
+    <div className="flex w-screen flex-col items-center justify-center">
+      <h1 className="mx-8 mt-8 text-center text-2xl">
+        Search Results for: {currentSearchTerm}
+      </h1>
+      <div className="flex w-screen items-center justify-center">
+        <div className="mb-12 mt-12 flex w-full flex-wrap items-center justify-center gap-8 md:mx-4 lg:mx-8 lg:gap-14">
+          {imageItems}
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/components/searchResults/SearchResultsPage.tsx
+++ src/components/searchResults/SearchResultsPage.tsx
@@ -1,33 +1,15 @@
-import { useAppSelector } from '../../redux/hooks/reduxHooks'
-import { ImageItem } from './ImageItem'
 import { NavBar } from '../navBar/NavBar'
 import { SearchNav } from '../../ui/ui-index'
+import { SearchResults } from './SearchResults'
 
 export const SearchResultsPage = () => {
-  const searchResults = useAppSelector((state) => state.dataReducer.imageData)
-  const currentSearchTerm = useAppSelector(
-    (state) => state.dataReducer.searchTerm
-  )
-
-  const imageItems = searchResults.map((imageData) => {
-    const { _id } = imageData
-    return <ImageItem key={_id} imageData={imageData} />
-  })
-
   return (
-    <section className="min-h-screen">
+    <>
       <NavBar />
       <SearchNav />
-      <div className="flex w-screen flex-col items-center justify-center">
-        <h1 className="mx-8 mt-8 text-center text-3xl">
-          Search Results for: {currentSearchTerm}
-        </h1>
-        <div className="flex w-screen items-center justify-center">
-          <div className="mb-12 mt-12 flex w-full flex-wrap items-center justify-center gap-8 md:mx-4 lg:mx-8 lg:gap-14">
-            {imageItems}
-          </div>
-        </div>
-      </div>
-    </section>
+      <section className="min-h-screen">
+        <SearchResults />
+      </section>
+    </>
   )
 }
