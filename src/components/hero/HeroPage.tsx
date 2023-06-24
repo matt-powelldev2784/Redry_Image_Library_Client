e53c71817841a6228f0914a3b509374b5e2d5a53
Matthew Powell <matt.powell2784@gmail.com>
2023-06-24 13:24:43 +0100

    redesign hero page

--- src/components/hero/HeroPage.tsx
+++ src/components/hero/HeroPage.tsx
@@ -23,47 +23,58 @@ export const HeroPage = () => {
   return (
     <>
       <NavBar />
-      <section className="relative flex h-screen min-h-[800px] w-screen min-w-[320px] items-start justify-center md:items-center">
-        <img
-          src="/light_circle_trans2.png"
-          alt=""
-          className="absolute top-40 z-10 w-48 opacity-40 md:top-80 md:hidden"
-        />
-        <img
-          src="/world_green_light_extended5.jpg"
-          alt="Green light bulb background"
-          className="absolute h-full w-full object-cover"
-        />
-
-        <div className="z-20 mt-20 flex w-full flex-col items-center justify-center md:m-0 lg:mx-16 lg:w-[40rem]">
+      <section className="relative flex min-h-screen w-screen min-w-[320px] flex-col items-center justify-start">
+        <div className="relative h-[200px] w-screen overflow-y-hidden md:h-[250px]">
           <img
-            src="/re_dry_logo_green_slogan.svg"
-            alt="Redry logo with slogan"
-            className="m-4 h-20 md:h-24"
+            src="/world_green_light_extended6.jpg"
+            alt="Green light bulb background"
+            className="absolute h-full w-full object-cover md:h-[400px]"
           />
 
-          <h1 className="mt-8 overflow-hidden text-center text-lg font-semibold md:text-3xl">
+          <div className="relative flex h-full w-screen items-center justify-center">
+            <form
+              onSubmit={handleButtonSearch}
+              className="relative flex flex-col items-center justify-center md:w-[500px]"
+            >
+              <div className="flex w-[90%] items-center justify-center ">
+                <SearchInput
+                  wrapperClassNames=""
+                  inputClassNames="px-3 py-2 text-center text-base"
+                  placeholderText="Search Image Library"
+                />
+              </div>
+              <div className="mt-2 flex w-full items-center justify-center">
+                <Button
+                  optionalClasses="relative w-[90%] rounded-xl px-4 py-1 text-xl font-semibold text-darkBlack"
+                  buttonText="Search"
+                  type="submit"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <div className="relative flex w-full flex-col items-center justify-center lg:mx-16 lg:w-[40rem]">
+          <h1 className="mt-6 overflow-hidden text-center text-lg font-semibold md:text-3xl">
             User Generated Image Library
           </h1>
-          <h2 className="mx-4 mt-4 text-center text-base md:w-[500px] md:text-xl lg:mx-0">
+
+          <h2 className="mx-4 mt-2 text-center text-base md:w-[500px] md:text-center md:text-xl lg:mx-0">
             Your destination for stunning, high-quality images. Explore our
             collection and find the perfect visuals for your next project.
           </h2>
 
-          <div className="mt-12 flex w-11/12 max-w-[500px] flex-col md:w-1/2 lg:w-[500px]">
-            <form onSubmit={handleButtonSearch}>
-              <SearchInput
-                inputClassNames="w-full rounded-xl px-3 py-2 text-center text-base"
-                placeholderText="Search Image Library"
-              />
+          <img
+            src="/light_circle_trans2.png"
+            alt=""
+            className="absolute mt-40 h-48 opacity-40 md:mt-36"
+          />
 
-              <Button
-                optionalClasses="w-full my-2 mt-4 rounded-xl px-4 py-1 text-xl font-semibold text-darkBlack"
-                buttonText="Search"
-                type="submit"
-              />
-            </form>
-          </div>
+          <img
+            src="/re_dry_logo_green_slogan.svg"
+            alt="Redry logo with slogan"
+            className="z-20 mb-16 mt-20 h-16 md:mt-16 md:h-24"
+          />
         </div>
       </section>
     </>
