8e7f4fa141e5104767ab19eb6e5d91c8b836aa03
Matthew Powell <matt.powell2784@gmail.com>
2023-06-27 13:45:43 +0100

    remove unused vard

--- src/components/homePage/components/__tests__/HomePageText.test.tsx
+++ src/components/homePage/components/__tests__/HomePageText.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { HomePageText } from '../HomePageText'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
