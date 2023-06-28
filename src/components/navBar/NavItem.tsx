da951d777bd90a0bf2e9af45cd4451671e842344
Matthew Powell <matt.powell2784@gmail.com>
2023-06-28 13:14:32 +0100

    corrects centering in edge

--- src/components/navBar/NavItem.tsx
+++ src/components/navBar/NavItem.tsx
@@ -9,7 +9,7 @@ export const NavItem = ({ onClick, text, imgPath }: NavItemProps) => {
     <div className="flex items-center justify-center gap-4 md:gap-8">
       <button
         onClick={onClick}
-        className="flex flex-col items-center justify-center "
+        className="flex flex-col items-center justify-center"
       >
         <img className="h-7" src={imgPath} alt="" />
         <p className="hidden md:block">{text}</p>
