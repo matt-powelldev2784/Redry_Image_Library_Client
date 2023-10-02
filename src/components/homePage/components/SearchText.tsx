894dbd23cf9e4108acbef651ba2dc51c11c682da
Matthew Powell <matt.powell2784@gmail.com>
2023-10-02 16:36:50 +0100

    update user journey text

--- src/components/homePage/components/SearchText.tsx
+++ src/components/homePage/components/SearchText.tsx
@@ -1,10 +1,10 @@
 export const SearchText = () => {
   return (
-    <div className="relative mb-2 flex w-11/12 flex-col items-center justify-center overflow-hidden text-red-500 md:w-[30rem] lg:mx-16">
-      <p className="text-red overflow-hidden text-center text-sm">
-        Preview Instructions:
+    <div className="relative mb-2 flex w-11/12 flex-col items-center justify-center overflow-hidden rounded-lg bg-red-500 p-2 sm:hidden md:block md:w-[30rem]">
+      <p className="text-red overflow-hidden text-center text-white">
+        Suggested user journey for users browsing portfolio:
       </p>
-      <p className="overflow-hidden text-center text-sm">
+      <p className="overflow-hidden text-center text-white">
         Search for "happy" or "animal" to preview the search feature or click
         the upload image menu to upload your own image.
       </p>
